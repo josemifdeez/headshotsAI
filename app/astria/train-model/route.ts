8a089f162b1200437bd48879f7e147c669b12ada
josemifdeez <josemifdeez@gmail.com>
2025-03-24 21:30:30 +0100

    Nueva URL

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -144,10 +144,10 @@ export async function POST(request: Request) {
 
   try {
 
-    const trainWebhook = `https://fotosia.vercel.app/astria/train-webhook`;
+    const trainWebhook = `https://www.sesionesfotosia.com/astria/train-webhook`;
     const trainWebhookWithParams = `${trainWebhook}?user_id=${user.id}&model_id=${modelId}&webhook_secret=${appWebhookSecret}`;
 
-    const promptWebhook = `https://fotosia.vercel.app/astria/prompt-webhook`;
+    const promptWebhook = `https://www.sesionesfotosia.com/astria/prompt-webhook`;
     const promptWebhookWithParams = `${promptWebhook}?user_id=${user.id}&&model_id=${modelId}&webhook_secret=${appWebhookSecret}`;
 
     const API_KEY = astriaApiKey;
